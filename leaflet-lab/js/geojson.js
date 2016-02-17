f8862826073c22b4688ab2dc1682d6935c2f1ad6
schuylerwilliams <Student@SHC-380-017.shc.wisc.edu>
2016-02-17 15:45:05 -0600

    Module 4 Activity Complete

--- leaflet-lab/js/geojson.js
+++ leaflet-lab/js/geojson.js
@@ -4,11 +4,22 @@ L.mapbox.accessToken = 'pk.eyJ1Ijoic2t5d2lsbGlhbXMiLCJhIjoibUI4TlByNCJ9.9UuhBU3E
 // initialize the map, using mapbox streets default
 var map = L.mapbox.map('map', 'mapbox.streets').setView([38, -95], 4);
 
-// use the mapbox omnivore plugin to display csv data as a geojson
-omnivore.csv('/mapbox.js/assets/data/airports.csv').addTo(map);
+// style the marker symbols
+var markerOptions = {
+		radius: 8,
+		fillColor: "orange",
+		color: "#000",
+		weight: 1,
+		opacity: 1,
+		fillOpacity: 0.8
+};
 
-/* I couldn't find the megacities.geojson file at learn@uw, so I could used omnivore and mapbox instead of leaflet
-	mapbox is actually built on top of leaflet, so almost every function is exactly the same, with a few added functions out
-	of the box, I prefer working wit mapbox.js instead of leaflet as they make it much more simple to use
-	with the mapbox styles
-*/
+
+// add megaCities geojson feature to map
+L.geoJson(megaCities, {
+	// use pointToLayer to make circles from points
+	pointToLayer: function (feature, latlng) {
+		return L.circleMarker(latlng, markerOptions);
+	}
+}).addTo(map);
+
